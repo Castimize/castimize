3e87be73c2816b6e529dfe3920367a3ac6d82853
matthijsbon <matthbon@hotmail.com>
2024-09-19 16:39:31 +0200

    - checking the price calc testing

--- app/Http/Controllers/Api/V1/PricesApiController.php
+++ app/Http/Controllers/Api/V1/PricesApiController.php
@@ -3,21 +3,21 @@
 namespace App\Http\Controllers\Api\V1;
 
 use App\Http\Resources\CalculatedPriceResource;
-use App\Http\Resources\UserResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
-use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class PricesApiController extends ApiController
 {
     /**
      * @param Request $request
+     * @return mixed[]
      */
     public function calculatePrice(Request $request)
     {
         abort_if(Gate::denies('viewPricing'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
-        return (new CalculatedPriceResource(null))->setRequestData($request);
+        $calculatedPriceResource = new CalculatedPriceResource();
+        return $calculatedPriceResource->setRequestData($request)->toArray();
     }
 }
