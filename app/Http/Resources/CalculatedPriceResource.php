c5d0c13a6efd5340305d93abb08cdb715350f648
matthijsbon <matthbon@hotmail.com>
2024-09-19 16:36:11 +0200

    - checking the price calc

---
+++ app/Http/Resources/CalculatedPriceResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CalculatedPriceResource extends JsonResource
+{
+    private $requestData;
+
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        $data = $this->requestData->toArray();
+        $data['argument_1'] = 10.00;
+        $data['argument_6']['p3d_estimated_price'] = 10.00;
+
+        return $data;
+    }
+
+    public function setRequestData(Request $request): static
+    {
+        $this->requestData = $request;
+        return $this;
+    }
+}
