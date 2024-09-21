2ff1bbd1d722df29344d2371376fc49a12d63246
matthijsbon <matthbon@hotmail.com>
2024-09-21 08:02:27 +0200

    - Log it again

--- app/Http/Controllers/Api/V1/PricesApiController.php
+++ app/Http/Controllers/Api/V1/PricesApiController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Api\V1;
 use App\Http\Resources\CalculatedPriceResource;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class PricesApiController extends ApiController
@@ -15,6 +16,7 @@ class PricesApiController extends ApiController
      */
     public function calculatePrice(Request $request)
     {
+        Log::info(print_r($request->toArray(), true));
         abort_if(Gate::denies('viewPricing'), Response::HTTP_FORBIDDEN, '403 Forbidden');
 
         return (new CalculatedPriceResource(null))->toArray($request);
