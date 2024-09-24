06506c8164be20e471d128293f6b106757c39488
matthijsbon <matthbon@hotmail.com>
2024-09-24 11:16:59 +0200

    - enable signature check

--- app/Http/Middleware/ValidateWcWebhookSignature.php
+++ app/Http/Middleware/ValidateWcWebhookSignature.php
@@ -18,19 +18,17 @@ class ValidateWcWebhookSignature
     {
         Log::info($request->header());
         Log::info(print_r($request->all(), true));
-//        $signature = $request->header('x-wc-webhook-signature');
-//        if (empty($signature)) {
-//            return response(['Invalid key'], 401);
-//        }
-//
-//        $payload = $request->getContent();
-//        $calculated_hmac = base64_encode(hash_hmac('sha256', $payload, env('WOOCOMMERCE_KEY'), true));
-//
-//        if ($signature != $calculated_hmac) {
-//            return response(['Invalid payload'], 401);
-//        }
+        $signature = $request->header('x-wc-webhook-signature');
+        if (empty($signature)) {
+            return response(['Invalid key'], 401);
+        }
 
+        $payload = $request->getContent();
+        $calculated_hmac = base64_encode(hash_hmac('sha256', $payload, env('WOOCOMMERCE_KEY'), true));
 
+        if ($signature != $calculated_hmac) {
+            return response(['Invalid payload'], 401);
+        }
 
         return $next($request);
     }
