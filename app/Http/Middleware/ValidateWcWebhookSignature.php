2a88d6e075f38725f5e79ee21f21179582584af9
matthijsbon <matthbon@hotmail.com>
2024-09-24 08:43:20 +0200

    - log request

--- app/Http/Middleware/ValidateWcWebhookSignature.php
+++ app/Http/Middleware/ValidateWcWebhookSignature.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 use Symfony\Component\HttpFoundation\Response;
 
 class ValidateWcWebhookSignature
@@ -15,6 +16,7 @@ class ValidateWcWebhookSignature
      */
     public function handle(Request $request, Closure $next): Response
     {
+        Log::info(print_r($request->all(), true));
         $signature = $request->header('X-WP-Webhook-Signature');
         if (empty($signature)) {
             return response(['Invalid key'], 401);
