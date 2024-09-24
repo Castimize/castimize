f8484eb6097d6ef2379a5f0b224eab908fa61844
matthijsbon <matthbon@hotmail.com>
2024-09-24 16:34:17 +0200

    - also do upload haha

--- app/Models/Upload.php
+++ app/Models/Upload.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -25,14 +26,10 @@ class Upload extends Model
         'order_id',
         'material_id',
         'customer_id',
-        'manufacturer_id',
         'currency_id',
         'name',
         'file_name',
-        'customer_first_name',
-        'customer_last_name',
         'material_name',
-        'manufacturer_name',
         'model_volume_cc',
         'model_x_length',
         'model_y_length',
@@ -40,28 +37,13 @@ class Upload extends Model
         'model_box_volume',
         'model_surface_area_cm2',
         'model_parts',
-        'exceeds_volume',
-        'exceeds_number_of_parts',
-        'exceeds_file_size',
-        'price',
+        'quantity',
+        'subtotal',
+        'subtotal_tax',
+        'total',
+        'total_tax',
         'currency_code',
         'customer_lead_time',
-//        'bulk_discount_price',
-//        'in_queue',
-//        'accepted_at',
-//        'rejected_at',
-//        'rejection_reason',
-//        'reprint_at',
-//        'available_for_shipping',
-//        'in_shipping',
-//        'manufacturer_shipment_id',
-//        'customer_shipment_id',
-//        'in_transit_to_dc',
-//        'contract_date',
-//        'arrived_at',
-//        'manufacturing_costs',
-//        'model_minimum',
-//        'bulk_discount_costs',
     ];
 
     /**
@@ -75,13 +57,51 @@ class Upload extends Model
             'created_at' => 'datetime',
             'updated_at' => 'datetime',
             'deleted_at' => 'datetime',
-            'in_queue' => 'datetime',
-            'accepted_at' => 'datetime',
-            'rejected_at' => 'datetime',
-            'reprint_at' => 'datetime',
-            'contract_date' => 'datetime',
-            'arrived_at' => 'datetime',
         ];
+    }
+
+    /**
+     * Interact with  subtotal
+     */
+    protected function subtotal(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100,
+        );
+    }
+
+    /**
+     * Interact with  subtotal_tax
+     */
+    protected function subtotalTax(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100,
+        );
+    }
+
+    /**
+     * Interact with  subtotal
+     */
+    protected function total(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100,
+        );
+    }
+
+    /**
+     * Interact with  subtotal_tax
+     */
+    protected function totalTax(): Attribute
+    {
+        return Attribute::make(
+            get: fn ($value) => $value / 100,
+            set: fn ($value) => $value * 100,
+        );
     }
 
     /**
@@ -111,32 +131,8 @@ class Upload extends Model
     /**
      * @return BelongsTo
      */
-    public function manufacturer(): BelongsTo
-    {
-        return $this->belongsTo(Manufacturer::class);
-    }
-
-    /**
-     * @return BelongsTo
-     */
     public function currency(): BelongsTo
     {
         return $this->belongsTo(Currency::class);
-    }
-
-    /**
-     * @return BelongsTo
-     */
-    public function manufacturerShipment(): BelongsTo
-    {
-        return $this->belongsTo(ManufacturerShipment::class);
-    }
-
-    /**
-     * @return BelongsTo
-     */
-    public function customerShipment(): BelongsTo
-    {
-        return $this->belongsTo(CustomerShipment::class);
     }
 }
