ed055e432f189a5496ee038183aa8f17b4a39126
matthijsbon <matthbon@hotmail.com>
2024-09-25 10:36:34 +0200

    - order better saving customer lead time

--- app/Http/Middleware/ValidateWcWebhookSignature.php
+++ app/Http/Middleware/ValidateWcWebhookSignature.php
@@ -18,16 +18,15 @@ class ValidateWcWebhookSignature
     {
         Log::info($request->header());
         Log::info(print_r($request->all(), true));
-        $signatureWc = $request->header('x-wc-webhook-signature');
-        $signatureWp = $request->header('x-wp-webhook-signature');
-        if (empty($signatureWc) && empty($signatureWp)) {
+        $signature = $request->header('x-wc-webhook-signature');
+        if (empty($signature)) {
             return response(['Invalid key'], 401);
         }
 
         $payload = $request->getContent();
         $calculated_hmac = base64_encode(hash_hmac('sha256', $payload, env('WOOCOMMERCE_KEY'), true));
 
-        if ($signatureWc != $calculated_hmac && $signatureWp != $calculated_hmac) {
+        if ($signature != $calculated_hmac) {
             Log::info('Invalid payload');
             return response(['Invalid payload'], 401);
         }
