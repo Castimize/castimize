17e89dc6045f791c3c135b43fce4c012b1a4b234
matthbon <matthbon@hotmail.com>
2024-10-22 05:37:52 +0200

    request logger around webhook calls

--- routes/web.php
+++ routes/web.php
@@ -1,16 +1,19 @@
 <?php
 
+use App\Http\Middleware\RequestLogger;
 use App\Http\Middleware\VerifyShippoWebhookSignature;
 use App\Http\Middleware\VerifyStripeWebhookSignature;
 use Illuminate\Support\Facades\Route;
 
-Route::group([
-    'namespace' => 'App\Http\Controllers',
-], function () {
-    Route::post('/webhooks/payment/stripe/callback', 'Webhooks\Payments\StripeWebhookController@handleWebhook')
-        ->name('webhooks.payment.stripe.callback')
-        ->middleware(VerifyStripeWebhookSignature::class);
-    Route::post('/webhooks/shipping/shippo/callback', 'Webhooks\Shipping\ShippoWebhookController@handleWebhook')
-        ->name('webhooks.shipping.shippo.callback')
-        ->middleware(VerifyShippoWebhookSignature::class);
+Route::group(['middleware' => [RequestLogger::class]], function () {
+    Route::group([
+        'namespace' => 'App\Http\Controllers',
+    ], function () {
+        Route::post('/webhooks/payment/stripe/callback', 'Webhooks\Payments\StripeWebhookController@handleWebhook')
+            ->name('webhooks.payment.stripe.callback')
+            ->middleware(VerifyStripeWebhookSignature::class);
+        Route::post('/webhooks/shipping/shippo/callback', 'Webhooks\Shipping\ShippoWebhookController@handleWebhook')
+            ->name('webhooks.shipping.shippo.callback')
+            ->middleware(VerifyShippoWebhookSignature::class);
+    });
 });
