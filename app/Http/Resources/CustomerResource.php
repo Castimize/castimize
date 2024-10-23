1a0fa1725ef00ede121e897f8e8c65f558f825c3
matthbon <matthbon@hotmail.com>
2024-10-23 11:22:42 +0200

    Fix customer resource addresses

--- app/Http/Resources/CustomerResource.php
+++ app/Http/Resources/CustomerResource.php
@@ -22,6 +22,9 @@ class CustomerResource extends JsonResource
             $lastOrderArray['created_at'] = $lastOrder->created_at;
         }
 
+        $billingAddress = $this->addresses()->wherePivot('default_billing', 1)->first();
+        $shippingAddress = $this->addresses()->wherePivot('default_shipping', 1)->first();
+
         return [
             'wp_id' => $this->wp_id,
             'email' => $this->email ?? $this->user?->email,
@@ -33,8 +36,8 @@ class CustomerResource extends JsonResource
             'avatar' => $this->user?->avatar ? env('CLOUDFLARE_R2_URL') . $this->user->avatar : null,
             'date_created' => $this->created_at,
             'date_modified' => $this->updated_at,
-            'billing' => (new AddressResource($this->addresses()->wherePivot('default_billing', 1)->first()))->toArray($request),
-            'shipping' => (new AddressResource($this->addresses()->wherePivot('default_shipping', 1)->first())),
+            'billing' => $billingAddress ? (new AddressResource($billingAddress))->toArray($request) : [],
+            'shipping' => $shippingAddress ? (new AddressResource($shippingAddress))->toArray($request) : [],
         ];
     }
 }
