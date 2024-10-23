c922409563ed8b094dd08f003a4015f441f11664
matthbon <matthbon@hotmail.com>
2024-10-23 10:07:52 +0200

    Fix customer empty address

--- app/Services/Admin/CustomersService.php
+++ app/Services/Admin/CustomersService.php
@@ -71,22 +71,24 @@ class CustomersService
         if ($request->shipping['address_1'] !== $request->billing['address_1']) {
             $shippingAddress = $this->createAddress($request->shipping);
 
-            $pivotData = [
-                'default_billing' => 1,
-                'default_shipping' => 0,
-                'contact_name' => sprintf('%s %s', $request->billing['first_name'], $request->billing['last_name']),
-                'phone' => $request->billing['phone'] ?? null,
-            ];
-            $customer->addresses()->attach($billingAddress, $pivotData);
+            if ($shippingAddress !== null) {
+                $pivotData = [
+                    'default_billing' => 1,
+                    'default_shipping' => 0,
+                    'contact_name' => sprintf('%s %s', $request->billing['first_name'], $request->billing['last_name']),
+                    'phone' => $request->billing['phone'] ?? null,
+                ];
+                $customer->addresses()->attach($billingAddress, $pivotData);
 
-            $pivotData = [
-                'default_billing' => 0,
-                'default_shipping' => 1,
-                'contact_name' => sprintf('%s %s', $request->shipping['first_name'], $request->shipping['last_name']),
-                'phone' => $request->shiping['phone'] ?? null,
-            ];
-            $customer->addresses()->attach($shippingAddress, $pivotData);
-        } else {
+                $pivotData = [
+                    'default_billing' => 0,
+                    'default_shipping' => 1,
+                    'contact_name' => sprintf('%s %s', $request->shipping['first_name'], $request->shipping['last_name']),
+                    'phone' => $request->shiping['phone'] ?? null,
+                ];
+                $customer->addresses()->attach($shippingAddress, $pivotData);
+            }
+        } else if ($billingAddress !== null) {
             $pivotData = [
                 'default_billing' => 1,
                 'default_shipping' => 1,
@@ -110,32 +112,42 @@ class CustomersService
         $customer->email = $request->email ?? $request->billing['email'] ?? null;
         $customer->save();
 
-        $billingAddress = $customer->addresses->where('postal_code', $request->billing['postcode'])->where('address_line1', $request->billing['address_1'] ?? '-')->first();
-        $shippingAddress = $customer->addresses->where('postal_code', $request->shipping['postcode'])->where('address_line1', $request->shipping['address_1'] ?? '-')->first();
+        $billingAddress = null;
+        $shippingAddress = null;
+        if ($request->billing['postcode'] !== null) {
+            $billingAddress = $customer->addresses->where('postal_code', $request->billing['postcode'])->where('address_line1', $request->billing['address_1'] ?? '-')->first();
+        }
+        if ($request->shipping['postcode'] !== null) {
+            $shippingAddress = $customer->addresses->where('postal_code', $request->shipping['postcode'])->where('address_line1', $request->shipping['address_1'] ?? '-')->first();
+        }
         if ($billingAddress === null) {
             $billingAddress = $this->createAddress($request->billing);
-            $customer->addresses()->wherePivot('default_billing', 1)->update(['default_billing' => 0]);
+            if ($billingAddress !== null) {
+                $customer->addresses()->wherePivot('default_billing', 1)->update(['default_billing' => 0]);
+            }
         }
 
         if ($shippingAddress === null || $shippingAddress->address_line1 !== $billingAddress->address_line1) {
             $shippingAddress = $this->createAddress($request->shipping);
-            $customer->addresses()->wherePivot('default_shipping', 1)->update(['default_shipping' => 0]);
-            $pivotData = [
-                'default_billing' => 1,
-                'default_shipping' => 0,
-                'contact_name' => sprintf('%s %s', $request->billing['first_name'], $request->billing['last_name']),
-                'phone' => $request->billing['phone'] ?? null,
-            ];
-            $customer->addresses()->attach($billingAddress, $pivotData);
+            if ($shippingAddress !== null) {
+                $customer->addresses()->wherePivot('default_shipping', 1)->update(['default_shipping' => 0]);
+                $pivotData = [
+                    'default_billing' => 1,
+                    'default_shipping' => 0,
+                    'contact_name' => sprintf('%s %s', $request->billing['first_name'], $request->billing['last_name']),
+                    'phone' => $request->billing['phone'] ?? null,
+                ];
+                $customer->addresses()->attach($billingAddress, $pivotData);
 
-            $pivotData = [
-                'default_billing' => 0,
-                'default_shipping' => 1,
-                'contact_name' => sprintf('%s %s', $request->shipping['first_name'], $request->shipping['last_name']),
-                'phone' => $request->shipping['phone'] ?? null,
-            ];
-            $customer->addresses()->attach($shippingAddress, $pivotData);
-        } else {
+                $pivotData = [
+                    'default_billing' => 0,
+                    'default_shipping' => 1,
+                    'contact_name' => sprintf('%s %s', $request->shipping['first_name'], $request->shipping['last_name']),
+                    'phone' => $request->shipping['phone'] ?? null,
+                ];
+                $customer->addresses()->attach($shippingAddress, $pivotData);
+            }
+        } else if ($billingAddress !== null) {
             $pivotData = [
                 'default_billing' => 1,
                 'default_shipping' => 1,
@@ -150,42 +162,45 @@ class CustomersService
 
     private function createAddress(array $input)
     {
-        $country = Country::where('alpha2', strtolower($input['country']))->first();
-        if ($country === null) {
-            $country = Country::where('alpha2', 'nl')->first();
-        }
+        if ($input['postcode'] !== null) {
+            $country = Country::where('alpha2', strtolower($input['country']))->first();
+            if ($country === null) {
+                $country = Country::where('alpha2', 'nl')->first();
+            }
 
-        $stateName = $input['state'];
-        $state = null;
-        if ($stateName) {
-            $state = State::firstOrCreate(
-                ['name' => $stateName],
-                ['name' => $stateName, 'slug' => Str::slug($stateName), 'country_id' => $country->id]
-            );
-        }
-        $cityName = $input['city'];
-        $city = null;
-        if ($cityName) {
-            $city = City::firstOrCreate(
-                ['name' => $cityName],
-                ['name' => $cityName, 'slug' => Str::slug($cityName), 'state_id' => $state?->id, 'country_id' => $country->id]
-            );
-        }
+            $stateName = $input['state'];
+            $state = null;
+            if ($stateName) {
+                $state = State::firstOrCreate(
+                    ['name' => $stateName],
+                    ['name' => $stateName, 'slug' => Str::slug($stateName), 'country_id' => $country->id]
+                );
+            }
+            $cityName = $input['city'];
+            $city = null;
+            if ($cityName) {
+                $city = City::firstOrCreate(
+                    ['name' => $cityName],
+                    ['name' => $cityName, 'slug' => Str::slug($cityName), 'state_id' => $state?->id, 'country_id' => $country->id]
+                );
+            }
 
-        $address = Address::where('postal_code', $input['postcode'])->where('address_line1', $input['address_1'] ?? '-')->first();
-        if ($address === null) {
-            $address = Address::create([
-                'address_line1' => $input['address_1'],
-                'address_line2' => $input['address_2'] ?? null,
-                'postal_code' => $input['postcode'],
-                'city_id' => $city?->id,
-                'state_id' => $state?->id,
-                'country_id' => $country->id,
-                'created_by' => 1,
-                'updated_by' => 1,
-            ]);
-        }
+            $address = Address::where('postal_code', $input['postcode'])->where('address_line1', $input['address_1'] ?? '-')->first();
+            if ($address === null) {
+                $address = Address::create([
+                    'address_line1' => $input['address_1'],
+                    'address_line2' => $input['address_2'] ?? null,
+                    'postal_code' => $input['postcode'],
+                    'city_id' => $city?->id,
+                    'state_id' => $state?->id,
+                    'country_id' => $country->id,
+                    'created_by' => 1,
+                    'updated_by' => 1,
+                ]);
+            }
 
-        return $address;
+            return $address;
+        }
+        return null;
     }
 }
