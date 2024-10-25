99eba056e23a48d8e37bb662d059f0c52833e3a4
matthbon <matthbon@hotmail.com>
2024-10-25 06:30:31 +0200

    - add store from model api job

---
+++ app/Jobs/StoreModelFromApi.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Services\Admin\ModelsService;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+
+class StoreModelFromApi implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(protected $request)
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        (new ModelsService())->storeModelFromApi($this->request);
+    }
+}
