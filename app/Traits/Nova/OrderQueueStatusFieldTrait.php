cb65a515cb223dbe3df261d343de11b0031e45d7
matthbon <matthbon@hotmail.com>
2024-11-25 12:21:49 +0100

    - on scheduel also on canceled

--- app/Traits/Nova/OrderQueueStatusFieldTrait.php
+++ app/Traits/Nova/OrderQueueStatusFieldTrait.php
@@ -76,9 +76,6 @@ trait OrderQueueStatusFieldTrait
             return '<span data-toggle="tooltip" data-placement="top" title="' . __('Behind schedule') . '"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="rgb(239 68 68)" stroke-width="2"><path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>';
         })
             ->asHtml()
-            ->hideOnExport()
-            ->canSee(function () {
-                return !in_array($this->getLastStatus()?->slug, ['canceled']);
-            });
+            ->hideOnExport();
     }
 }
