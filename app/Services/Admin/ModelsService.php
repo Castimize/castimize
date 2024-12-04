d12f912c4aee3febdf4ab9d435bb0b50360b71eb
matthbon <matthbon@hotmail.com>
2024-12-04 18:32:21 +0100

    - fix saving thumb

--- app/Services/Admin/ModelsService.php
+++ app/Services/Admin/ModelsService.php
@@ -4,6 +4,8 @@ namespace App\Services\Admin;
 
 use App\Models\Material;
 use App\Models\Model;
+use Exception;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
 class ModelsService
@@ -11,7 +13,7 @@ class ModelsService
     public function storeModelFromApi($request, ?int $customerId = null): Model|null
     {
         $material = Material::where('wp_id', $request->wp_id)->first();
-        $fileName = env('APP_SITE_STL_UPLOAD_DIR') . $request->file_name;
+        $fileName = $request->file_name;
         $categories = null;
         if ($request->has('categories')) {
             $categories = [];
@@ -20,6 +22,30 @@ class ModelsService
                     'category' => $category,
                 ];
             }
+        }
+
+        $fileNameThumb = sprintf('%s%s.thumb.png', env('APP_SITE_STL_UPLOAD_DIR'), str_replace('_resized', '', $fileName));
+        $fileName = sprintf('%s%s', env('APP_SITE_STL_UPLOAD_DIR'), $fileName);
+        $fileUrl = sprintf('%s/%s', env('APP_SITE_URL'), $fileName);
+        $fileThumb = sprintf('%s/%s', env('APP_SITE_URL'), $fileNameThumb);
+        $fileHeaders = get_headers($fileUrl);
+        $withoutResizedFileName = str_replace('_resized', '', $fileName);
+
+        try {
+            // Check files exists on local storage of site and not on R2
+            if (!str_contains($fileHeaders[0], '404') && !Storage::disk('r2')->exists($fileName)) {
+                Storage::disk('r2')->put($fileName, file_get_contents($fileUrl));
+            }
+            // Check files exists on local storage of site and not on R2 (without resized
+            if (!str_contains($fileHeaders[0], '404') && !Storage::disk('r2')->exists($withoutResizedFileName)) {
+                Storage::disk('r2')->put($withoutResizedFileName, file_get_contents($fileUrl));
+            }
+            // Check file thumb exists on local storage of site and not on R2
+            if (!str_contains($fileHeaders[0], '404') && !Storage::disk('r2')->exists($fileNameThumb)) {
+                Storage::disk('r2')->put($fileNameThumb, file_get_contents($fileThumb));
+            }
+        } catch (Exception $e) {
+            Log::error($e->getMessage() . PHP_EOL . $e->getTraceAsString());
         }
 
         return Model::create([
