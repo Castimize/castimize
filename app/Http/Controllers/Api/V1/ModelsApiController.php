7d223e5a778730fc094c1c0b5d0b749fece32584
matthbon <matthbon@hotmail.com>
2024-12-07 19:15:42 +0100

    fix customer model

--- app/Http/Controllers/Api/V1/ModelsApiController.php
+++ app/Http/Controllers/Api/V1/ModelsApiController.php
@@ -113,7 +113,7 @@ class ModelsApiController extends ApiController
             [$materialId, $materialName] = explode('. ', $upload['3dp_options']['material_name']);
             $model = $customer->models->where('file_name', 'wp-content/uploads/p3d/' . $upload['3dp_options']['model_name'])
                 ->where('material_id', $upload['3dp_options']['material_id'] ?? $materialId)
-                ->where('model_scale', $upload['3dp_options']['model_stats_raw']['model']['scale'])
+                ->where('model_scale', $upload['3dp_options']['scale'])
                 ->first();
 
             $newUploads[$itemKey] = $upload;
