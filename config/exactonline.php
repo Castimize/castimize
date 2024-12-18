cfe4010062b8a6ba2cb4b2066769e31b49367847
matthbon <matthbon@hotmail.com>
2024-12-18 10:02:44 +0100

    - config exact

---
+++ config/exactonline.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    /**
+     * The Client ID of your Exact Online application.
+     */
+    'exact_client_id' => env('EXACT_CLIENT_ID'),
+
+    /**
+     * The Client Secret of your Exact Online application.
+     */
+    'exact_client_secret' => env('EXACT_CLIENT_SECRET'),
+
+    /**
+     * The Division of your Exact Online application.
+     */
+    'exact_division' => env('EXACT_DIVISION'),
+
+    /**
+     * The Client country code
+     */
+    'exact_country_code' => 'nl',
+
+    /**
+     * Callback url for exact online
+     */
+    'callback_url' => 'https://app.castimize.com/exact/oauth',
+
+    /**
+     * Webhook secret
+     */
+    'webhook_secret' => env('EXACT_WEBHOOK_SECRET'),
+];
