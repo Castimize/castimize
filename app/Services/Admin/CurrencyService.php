2d6c9c0e63856d7ee0640852a58de83b5af812f6
matthbon <matthbon@hotmail.com>
2025-01-19 07:34:16 +0100

    - use last currency history rate when not found and api not working

--- app/Services/Admin/CurrencyService.php
+++ app/Services/Admin/CurrencyService.php
@@ -5,6 +5,7 @@ namespace App\Services\Admin;
 use App\Models\CurrencyHistoryRate;
 use AshAllenDesign\LaravelExchangeRates\Classes\ExchangeRate;
 use Carbon\Carbon;
+use Exception;
 use Illuminate\Support\Facades\Log;
 
 class CurrencyService
@@ -37,14 +38,32 @@ class CurrencyService
             ->first();
 
         if ($currencyHistoricalRate) {
-            if ($currencyHistoricalRate->convert_currency === $from) {
-                return $amount / $currencyHistoricalRate->rate;
-            }
-            return $amount * $currencyHistoricalRate->rate;
+            return $this->calculateRate($currencyHistoricalRate, $from, $amount);
         }
 
         Log::info(sprintf('From: %s, To: %s, Amount: %s, Date: %s', $from, $to, $amount, $date->format('Y-m-d')));
 
-        return $this->exchangeRate->convert($amount, $from, $to, $date);
+        try {
+            return $this->exchangeRate->convert($amount, $from, $to, $date);
+        } catch (Exception $e) {
+            Log::error($e->getMessage());
+
+            $currencyHistoricalRate = CurrencyHistoryRate::where('base_currency', $baseCurrency)
+                ->where('convert_currency', $convertCurrency)
+                ->orderBy('historical_date', 'desc')
+                ->first();
+            if ($currencyHistoricalRate) {
+                return $this->calculateRate($currencyHistoricalRate, $from, $amount);
+            }
+            return $amount;
+        }
+    }
+
+    private function calculateRate($currencyHistoricalRate, string $from, float $amount): float
+    {
+        if ($currencyHistoricalRate->convert_currency === $from) {
+            return $amount / $currencyHistoricalRate->rate;
+        }
+        return $amount * $currencyHistoricalRate->rate;
     }
 }
