5808abefe8f34cee7218df5b4ed6c478a7775d04
matthbon <matthbon@hotmail.com>
2025-02-16 11:14:19 +0100

    - dd taxonomy

--- app/Http/Controllers/Api/V1/EtsyApiController.php
+++ app/Http/Controllers/Api/V1/EtsyApiController.php
@@ -20,6 +20,7 @@ class EtsyApiController extends ApiController
         $customer = Customer::find($customerId);
         $shopOwnerAuth = $customer->shopOwner->shopOwnerAuths->first();
         $taxonomy = $this->etsyService->getSellerTaxonomy($shopOwnerAuth);
+        dd($taxonomy);
         foreach ($taxonomy as $item) {
             if ($item->name === 'Prints') {
                 dd($item->children);
