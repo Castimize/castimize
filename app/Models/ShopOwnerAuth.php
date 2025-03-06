d54fa9bc7848e59e59b6bbb11be71dd2fe9cd2c4
matthbon <matthbon@hotmail.com>
2025-03-06 19:00:03 +0100

    - etsy 1.0 has many

--- app/Models/ShopOwnerAuth.php
+++ app/Models/ShopOwnerAuth.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Attributes\ObservedBy;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Venturecraft\Revisionable\RevisionableTrait;
 use Wildside\Userstamps\Userstamps;
@@ -53,5 +54,10 @@ class ShopOwnerAuth extends Model
     public function shopOwner(): BelongsTo
     {
         return $this->belongsTo(ShopOwner::class);
+    }
+
+    public function shopListingModels(): HasMany
+    {
+        return $this->hasMany(ShopListingModel::class);
     }
 }
