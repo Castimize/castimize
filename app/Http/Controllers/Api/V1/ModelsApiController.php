7504bf0f633fa78d8f8e1cedfc1e46a54530eb78
matthbon <matthbon@hotmail.com>
2025-08-03 17:38:02 +0200

    - no uploading because not needed

--- app/Http/Controllers/Api/V1/ModelsApiController.php
+++ app/Http/Controllers/Api/V1/ModelsApiController.php
@@ -141,6 +141,7 @@ class ModelsApiController extends ApiController
         LogRequestService::addResponse($request, $newUploads);
         return response()->json($newUploads);
     }
+
     public function store(int $customerId, Request $request): JsonResponse
     {
         ini_set('precision', 17);
