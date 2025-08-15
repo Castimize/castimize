52ddbf33fd4e432f3d88b1f61cc10423b9d35e04
matthbon <matthbon@hotmail.com>
2025-08-15 05:35:42 +0200

    - fix saving image?

--- app/Services/Admin/ModelsService.php
+++ app/Services/Admin/ModelsService.php
@@ -102,77 +102,6 @@ class ModelsService
         });
     }
 
-    public function getModelsPaginatedOld($request, Customer $customer)
-    {
-        // Page Length
-        $pageNumber = ( $request->start / $request->length ) + 1;
-        $pageLength = (int) $request->length;
-        $skip = (int) (($pageNumber - 1) * $pageLength);
-
-
-        $builder = $customer->models();
-
-        $builder->distinct([
-            'model_name',
-            'models.name',
-            'model_scale',
-//            'model_volume_cc',
-//            'model_surface_area_cm2',
-//            'model_box_volume',
-//            'model_x_length',
-//            'model_y_length',
-//            'model_z_length',
-        ]);
-
-        $recordsTotal = $builder->count();
-
-        if (! empty($request->search_value)) {
-            $builder
-                ->where(function ($query) use ($request) {
-                    $query->where('models.name', 'like', '%' . $request->search_value . '%')
-                        ->orWhere('model_name', 'like', '%' . $request->search_value . '%')
-                        ->orWhere('model_volume_cc', 'like', '%' . $request->search_value . '%')
-                        ->orWhere('model_x_length', 'like', '%' . $request->search_value . '%')
-                        ->orWhere('model_y_length', 'like', '%' . $request->search_value . '%')
-                        ->orWhere('model_z_length', 'like', '%' . $request->search_value . '%')
-                        ->orWhere('model_surface_area_cm2', 'like', '%' . $request->search_value . '%')
-                        ->orWhere('categories', 'like', '%' . $request->search_value . '%')
-                        ->orWhereHas('materials', function ($query) use ($request) {
-                            $query->where('name', 'like', '%' . $request->search_value . '%');
-                        });
-                });
-        }
-
-        if ($request->order_column) {
-            $mapper = [
-                'id' => 'id',
-                'name' => 'name',
-                'material' => 'material_name',
-                'material_volume' => 'model_volume_cc',
-                'surface_area' => 'model_surface_area_cm2',
-                'scale' => 'model_scale',
-                'categories' => 'categories',
-            ];
-
-            if (! isset($mapper, $request->order_column)) {
-                $builder->orderBy('id');
-            } elseif ($mapper[$request->order_column] === 'name') {
-                $builder->orderBy('model_name', $request->order_dir)
-                    ->orderBy('name', $request->order_dir);
-            } else {
-                $builder->orderBy($mapper[$request->order_column], $request->order_dir);
-            }
-        }
-
-        $recordsFiltered = $builder->count();
-        $models = $builder
-            ->skip($skip)
-            ->take($pageLength)
-            ->get();
-//        dd($skip, $pageLength, $recordsTotal, $recordsFiltered, $models);
-        return ['items' => ModelResource::collection($models), 'filtered' => $recordsFiltered, 'total' => $recordsTotal];
-    }
-
     public function storeModelFromApi($request, ?Customer $customer = null): Model|null
     {
         $scale = $request->scale ? number_format(round((float) $request->scale, 4), 4) : 1;
@@ -187,21 +116,6 @@ class ModelsService
                     'category' => $category,
                 ];
             }
-        }
-
-        if ($customer) {
-            $model = $customer->models->where('name', $request->original_file_name)
-                ->where('model_scale', $scale ?? 1)
-                ->first();
-
-            if ($model) {
-                return $model;
-            }
-        } else {
-            $model = Model::where('name', $request->original_file_name)
-                ->where('file_name', 'wp-content/uploads/p3d/' . $fileName)
-                ->where('model_scale', $scale ?? 1)
-                ->first();
         }
 
         $fileNameThumb = sprintf('%s%s.thumb.png', env('APP_SITE_STL_UPLOAD_DIR'), str_replace('_resized', '', $fileName));
@@ -226,6 +140,21 @@ class ModelsService
             }
         } catch (Exception $e) {
             Log::error($e->getMessage() . PHP_EOL . $e->getTraceAsString());
+        }
+
+        if ($customer) {
+            $model = $customer->models->where('name', $request->original_file_name)
+                ->where('model_scale', $scale ?? 1)
+                ->first();
+
+            if ($model) {
+                return $model;
+            }
+        } else {
+            $model = Model::where('name', $request->original_file_name)
+                ->where('file_name', 'wp-content/uploads/p3d/' . $fileName)
+                ->where('model_scale', $scale ?? 1)
+                ->first();
         }
 
         if ($model) {
