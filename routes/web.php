8f4cb30d520c023eb21c4d49030f59accf100d5b
matthbon <matthbon@hotmail.com>
2025-08-30 09:10:12 +0200

    - fix last error - composer lock - added coding standard

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\;
+use App\Http\Controllers\ExactOnlineController;
 use App\Http\Controllers\ModelsDownloadController;
 use App\Http\Controllers\PoLabelsDownloadController;
 use App\Http\Controllers\PrivateRejectionImageController;
@@ -14,10 +14,10 @@ use App\Services\Exact\ExactOnlineService;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('exact')->group(function () {
-    Route::get('connect', [\ExactOnlineController::class, 'appConnect'])->name('exact.connect');
-    Route::post('authorize', [\ExactOnlineController::class, 'appAuthorize'])->name('exact.authorize');
-    Route::get('oauth', [\ExactOnlineController::class, 'appCallback'])->name('exact.callback');
-    Route::post('callback-webhook', [\ExactOnlineController::class, 'appCallbackWebhook'])->name('exact.webhook');
+    Route::get('connect', [ExactOnlineController::class, 'appConnect'])->name('exact.connect');
+    Route::post('authorize', [ExactOnlineController::class, 'appAuthorize'])->name('exact.authorize');
+    Route::get('oauth', [ExactOnlineController::class, 'appCallback'])->name('exact.callback');
+    Route::post('callback-webhook', [ExactOnlineController::class, 'appCallbackWebhook'])->name('exact.webhook');
 
     Route::get('test', function () {
         dd((new ExactOnlineService)->getGlAccounts());
@@ -25,14 +25,12 @@ Route::prefix('exact')->group(function () {
 });
 
 Route::middleware(RequestLogger::class)->group(function () {
-    Route::group(function () {
-        Route::post('/webhooks/payment/stripe/callback', StripeWebhookController::class)
-            ->name('webhooks.payment.stripe.callback')
-            ->middleware(VerifyStripeWebhookSignature::class);
-        Route::post('/webhooks/shipping/shippo/callback', ShippoWebhookController::class)
-            ->name('webhooks.shipping.shippo.callback')
-            ->middleware(VerifyShippoWebhookSignature::class);
-    });
+    Route::post('/webhooks/payment/stripe/callback', StripeWebhookController::class)
+        ->name('webhooks.payment.stripe.callback')
+        ->middleware(VerifyStripeWebhookSignature::class);
+    Route::post('/webhooks/shipping/shippo/callback', ShippoWebhookController::class)
+        ->name('webhooks.shipping.shippo.callback')
+        ->middleware(VerifyShippoWebhookSignature::class);
 
     Route::prefix('providers')->group(function () {
         Route::get('etsy/oauth', EtsyAuthController::class)
