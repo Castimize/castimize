1581eafbe48b8c3b4d16383724a0e557cbbce180
matthbon <matthbon@hotmail.com>
2025-09-07 05:20:10 +0200

    mailgun service mail in create order

--- app/Jobs/CreateOrderFromDTO.php
+++ app/Jobs/CreateOrderFromDTO.php
@@ -6,6 +6,7 @@ use App\DTO\Order\OrderDTO;
 use App\Models\Order;
 use App\Services\Admin\LogRequestService;
 use App\Services\Admin\OrdersService;
+use App\Services\Mail\MailgunService;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Queue\Queueable;
 use Illuminate\Support\Facades\Log;
@@ -32,7 +33,7 @@ class CreateOrderFromDTO implements ShouldQueue
     /**
      * Execute the job.
      */
-    public function handle(): void
+    public function handle(MailgunService $mailgunService): void
     {
         $order = Order::where('wp_id', $this->orderDto->wpId)->first();
 
@@ -44,6 +45,20 @@ class CreateOrderFromDTO implements ShouldQueue
             $this->ordersService->storeOrderFromDto($this->orderDto);
         } catch (Throwable $e) {
             Log::error($e->getMessage().PHP_EOL.$e->getTraceAsString());
+            $title = 'Order creation failed for order number: '.$this->orderDto->orderNumber;
+            $mailgunService->send(
+                to: config('mail.from.address'),
+                subject: $title,
+                parameters: [
+                    'template' => 'order creation failed',
+                    'cc' => 'matthijs.bon1@gmail.com',
+                    'v:title' => $title,
+                    'v:order_number' => $this->orderDto->orderNumber,
+                    'v:error_message' => $e->getMessage(),
+                    'v:error_file' => $e->getFile(),
+                    'v:error_line' => $e->getLine(),
+                ],
+            );
         }
 
         try {
